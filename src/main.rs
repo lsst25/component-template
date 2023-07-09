21a698bd5b772592e79227e8ec1548673460979d
lsst25 <lsst25@gmail.com>
2023-07-09 21:11:44 +0300

    refactor: move template logic to separate module

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,7 @@
 mod template_type;
-mod utils;
+pub mod utils;
 
 use std::env;
-use std::fs::{self, File};
-use std::io::Error;
-use std::io::prelude::*;
-use std::path::Path;
-
 use template_type::Template;
 use utils::to_pascal_case;
 
@@ -24,44 +19,19 @@ fn main() -> std::io::Result<()> {
 
     match template_type {
         Template::Component => {
-            let path = template_type.get_path();
-
-            if Path::exists(Path::new(format!("{}/{}", path, name).as_str())) {
-                println!("Component {} already exists.", pascal_name);
-                return Ok(());
-            }
-
-            fs::create_dir(format!("{}/{}", path, name))?;
-
-            create_index_file(path, name)?;
-            create_component_file(path, name, &pascal_name)?;
-            create_stories_file(path, name, &pascal_name)?;
+            template_type.create_files(name)?;
 
             println!("Component {} created.", pascal_name);
         },
 
         Template::Entity => {
-            let path = template_type.get_path();
-
-            if Path::exists(Path::new(format!("{}/{}.entity.ts", path, name).as_str())) {
-                println!("Entity {} already exists.", pascal_name);
-                return Ok(());
-            }
-
-            create_entity_file(path, name, &pascal_name)?;
+            template_type.create_files(name)?;
 
             println!("Entity {} created.", pascal_name);
         },
 
         Template::Model => {
-            let path = template_type.get_path();
-
-            if Path::exists(Path::new(format!("{}/{}.model.ts", path, name).as_str())) {
-                println!("Model {} already exists.", pascal_name);
-                return Ok(());
-            }
-
-            create_model_file(path, name, &pascal_name)?;
+            template_type.create_files(name)?;
 
             println!("Model {} created.", pascal_name);
         },
@@ -71,58 +41,6 @@ fn main() -> std::io::Result<()> {
             return Ok(());
         }
     }
-
-    Ok(())
-}
-
-fn create_component_file(path: &str, name: &String, pascal_name: &String) -> Result<(), Error> {
-    let template = include_str!("./templates/component/component_template.tsx")
-        .replace("{component}", &pascal_name);
-
-    let mut file = File::create(format!("{path}/{name}/{name}.component.tsx"))?;
-    write!(file, "{}", template)?;
-
-    Ok(())
-}
-
-fn create_index_file(path: &str, name: &String) -> Result<(), Error> {
-    let template = include_str!("./templates/component/index_template.ts")
-        .replace("{component}", name);
-
-    let mut file = File::create(format!("{path}/{name}/index.ts"))?;
-    write!(file, "{}", template)?;
-
-    Ok(())
-}
-
-fn create_entity_file(path: &str, name: &String, pascal_name: &String) -> Result<(), Error> {
-    let template = include_str!("./templates/entity/entity_template.ts")
-        .replace("{pascal-name}", &pascal_name);
-
-    let mut file = File::create(format!("{path}/{name}.entity.ts"))?;
-    write!(file, "{}", template)?;
-
-    Ok(())
-}
-
-fn create_model_file(path: &str, name: &String, pascal_name: &String) -> Result<(), Error> {
-    let template = include_str!("./templates/model/model_template.ts")
-        .replace("{pascal-name}", &pascal_name)
-        .replace("{name}", &name);
-
-    let mut file = File::create(format!("{path}/{name}.model.ts"))?;
-    write!(file, "{}", template)?;
-
-    Ok(())
-}
-
-fn create_stories_file(path: &str, name: &String, pascal_name: &String) -> Result<(), Error> {
-    let template = include_str!("./templates/component/stories_template.tsx")
-        .replace("{pascal-name}", &pascal_name)
-        .replace("{name}", &name);
-
-    let mut file = File::create(format!("{path}/{name}/{name}.stories.tsx"))?;
-    write!(file, "{}", template)?;
 
     Ok(())
 }
--- src/template_type.rs
+++ src/template_type.rs
@@ -1,4 +1,8 @@
+use std::fs::{self, File};
+use std::io::Error;
+use std::io::prelude::*;
 use std::path::Path;
+use crate::utils::to_pascal_case;
 
 pub enum Template {
     Component,
@@ -19,7 +23,7 @@ impl From<&str> for Template {
 }
 
 impl Template {
-    pub fn get_path(&self) -> &str {
+    fn get_path(&self) -> &str {
         match self {
             Template::Component => if self.home_dir().exists() { "./ui" } else { "." },
             Template::Entity => if self.home_dir().exists() { "./entities" } else { "." },
@@ -36,4 +40,111 @@ impl Template {
             Template::Unknown => Path::new("."),
         }
     }
+
+    pub fn create_files(&self, name: &String) -> Result<(), Error> {
+        let pascal_name = to_pascal_case(name);
+
+        match self {
+            Template::Component => {
+                let path = self.get_path();
+
+                if Path::exists(Path::new(format!("{}/{}", path, name).as_str())) {
+                    println!("Component {} already exists.", pascal_name);
+                    return Ok(());
+                }
+
+                fs::create_dir(format!("{}/{}", path, name))?;
+
+                create_index_file(path, name)?;
+                create_component_file(path, name, &pascal_name)?;
+                create_stories_file(path, name, &pascal_name)?;
+
+                println!("Component {} created.", pascal_name);
+            },
+
+            Template::Entity => {
+                let path = self.get_path();
+
+                if Path::exists(Path::new(format!("{}/{}.entity.ts", path, name).as_str())) {
+                    println!("Entity {} already exists.", pascal_name);
+                    return Ok(());
+                }
+
+                create_entity_file(path, name, &pascal_name)?;
+
+                println!("Entity {} created.", pascal_name);
+            },
+
+            Template::Model => {
+                let path = self.get_path();
+
+                if Path::exists(Path::new(format!("{}/{}.model.ts", path, name).as_str())) {
+                    println!("Model {} already exists.", pascal_name);
+                    return Ok(());
+                }
+
+                create_model_file(path, name, &pascal_name)?;
+
+                println!("Model {} created.", pascal_name);
+            },
+
+            Template::Unknown => {
+                println!("Unknown template type.");
+            },
+        }
+
+        Ok(())
+    }
+}
+
+fn create_component_file(path: &str, name: &String, pascal_name: &String) -> Result<(), Error> {
+    let template = include_str!("./templates/component/component_template.tsx")
+        .replace("{component}", &pascal_name);
+
+    let mut file = File::create(format!("{path}/{name}/{name}.component.tsx"))?;
+    write!(file, "{}", template)?;
+
+    Ok(())
+}
+
+fn create_index_file(path: &str, name: &String) -> Result<(), Error> {
+    let template = include_str!("./templates/component/index_template.ts")
+        .replace("{component}", name);
+
+    let mut file = File::create(format!("{path}/{name}/index.ts"))?;
+    write!(file, "{}", template)?;
+
+    Ok(())
+}
+
+fn create_entity_file(path: &str, name: &String, pascal_name: &String) -> Result<(), Error> {
+    let template = include_str!("./templates/entity/entity_template.ts")
+        .replace("{pascal-name}", &pascal_name);
+
+    let mut file = File::create(format!("{path}/{name}.entity.ts"))?;
+    write!(file, "{}", template)?;
+
+    Ok(())
+}
+
+fn create_model_file(path: &str, name: &String, pascal_name: &String) -> Result<(), Error> {
+    let template = include_str!("./templates/model/model_template.ts")
+        .replace("{pascal-name}", &pascal_name)
+        .replace("{name}", &name);
+
+    let mut file = File::create(format!("{path}/{name}.model.ts"))?;
+    write!(file, "{}", template)?;
+
+    Ok(())
+}
+
+fn create_stories_file(path: &str, name: &String, pascal_name: &String) -> Result<(), Error> {
+    let template = include_str!("./templates/component/stories_template.tsx")
+        .replace("{pascal-name}", &pascal_name)
+        .replace("{name}", &name);
+
+    let mut file = File::create(format!("{path}/{name}/{name}.stories.tsx"))?;
+    write!(file, "{}", template)?;
+
+    Ok(())
 }
