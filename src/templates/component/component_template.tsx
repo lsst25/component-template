303ca51fe08e8d441e40a4b16c956ed4a32e0c8a
lsst25 <lsst25@gmail.com>
2023-07-19 19:37:22 +0300

    feat: improve component template

--- src/templates/component/component_template.tsx
+++ src/templates/component/component_template.tsx
@@ -1,6 +1,8 @@
-import { FC } from "react";
+import type { FC } from "react";
 
-export const $pascal_name$: FC = () => {
+export type $pascal_name$Props = {};
+
+export const $pascal_name$: FC = (props) => {
   return (
     <div>
       $pascal_name$ component works!
