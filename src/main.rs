6b89d5b68dc5d02cf7875e03a3695395d4505399
lsst25 <lsst25@gmail.com>
2023-07-27 22:01:59 +0300

    refactor: implement build method on Config struct

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,27 @@
 mod template_type;
 pub mod utils;
 
-use std::env;
+use std::{ env, process };
+use std::error::Error;
 use template_type::TemplateBuilder;
 
-fn main() -> std::io::Result<()> {
+fn main() -> Result<(), Box<dyn Error>> {
     let args: Vec<String> = env::args().collect();
-    if args.len() < 3 {
-        println!("Please provide a template and a name.");
-        return Ok(());
+
+    let config = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
+
+    if let Err(e) = run(config) {
+        println!("Application error: {e}");
+        process::exit(1);
     }
 
-    let config = parse_config(&args);
+    Ok(())
+}
 
+fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let builder = TemplateBuilder::new(&config.template, &config.name);
     builder.create_files()?;
 
@@ -24,9 +33,15 @@ struct Config {
     name: String,
 }
 
-fn parse_config(args: &[String]) -> Config {
-    let template = args[1].clone();
-    let name = args[2].clone();
+impl Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("Please provide a template and a name.");
+        }
 
-    Config { template, name }
+        let template = args[1].clone();
+        let name = args[2].clone();
+
+        Ok(Config { template, name })
+    }
 }
